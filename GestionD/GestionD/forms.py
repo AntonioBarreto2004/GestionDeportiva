6a1666971affadf46cca33e2ed7619acf88ee2df
AntonioBarreto2004 <110682839+AntonioBarreto2004@users.noreply.github.com>
2023-07-18 14:42:48 -0500

    Delete GestionD directory

--- GestionD/GestionD/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class RegisterForm(forms.Form):
-    username = forms.CharField(required=False)
-    email = forms.CharField(required=True)
-    password =forms.ChoiceField(required=False)
-    
