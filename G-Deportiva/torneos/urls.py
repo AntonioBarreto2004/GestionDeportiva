e4c4c7dc6e192a923f9ec6d8321355745c8b3d43
Johan Gruesso <133734302+johan-Camilo@users.noreply.github.com>
2023-08-04 17:25:48 -0500

    Delete G-Deportiva directory

--- G-Deportiva/torneos/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TournamentListCreateView, TournamentRetrieveUpdateDestroyView
-
-urlpatterns = [
-    path('tournaments/', TournamentListCreateView.as_view(), name='tournament-list-create'),
-    path('tournaments/<int:pk>/', TournamentRetrieveUpdateDestroyView.as_view(), name='tournament-retrieve-update-destroy'),
-]
