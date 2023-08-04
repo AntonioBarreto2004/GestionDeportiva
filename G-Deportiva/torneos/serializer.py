185686060cb6c4091678a703533d7baa60aeed17
AntonioBarreto2004 <110682839+AntonioBarreto2004@users.noreply.github.com>
2023-08-04 15:10:40 -0500

    Delete G-Deportiva directory

--- G-Deportiva/torneos/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Tournament
-
-class TournamentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tournament
-        fields = '__all__'
